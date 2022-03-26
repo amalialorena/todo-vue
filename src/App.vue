c5cfda0f618a985751b8c61c3451f0f9d336c74a
Amalia <amalialorenaferariu@gmail.com>
2022-03-26 08:01:56 +0100

    layout style modifications

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,19 @@ import ToDo from './components/ToDo.vue'
 </template>
 
 <style>
+body { 
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+* {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,36 +1,56 @@
 <template>
   <div class="container">
-    <h1>Welcome to my todo</h1>
-
-    <input
-      type="text"
-      v-model="inputTask"
-      placeholder="insert new task"
-      v-on:keyup.enter="createTask()"
-    />
-    <button @click="createTask()">add task</button>
-
-    <div class="task" v-for="(toDo, i) in taskArr" :key="i">
-      <div class="text-container">
-        <h3 :class="isTaskCompleted(toDo.id) ? 'completed-task' : ''">
-          {{ toDo.text }}
-        </h3>
+    <nav>
+      <img src="src/assets/img/myToDoLogo.svg" alt="" />
+    </nav>
+    <main>
+      <div class="title">
+        <img src="src/assets/img/tasksIcon.svg" alt="" />
+        <h2>Tasks</h2>
       </div>
+      <div class="input-container">
+        <input
+          type="text"
+          v-model="inputTask"
+          placeholder="Add a task"
+          v-on:keyup.enter="createTask()"
+        />
 
-      <div class="buttons">
-        <div>
+        <button class="add-task-btn" @click="createTask()">
+          <img src="src/assets/img/addTask.svg" alt="" />
+        </button>
+      </div>
+      <div class="task-count">Remaining tasks: {{ taskArr.length }}</div>
+
+      <div class="task" v-for="(toDo, i) in taskArr" :key="i">
+        <div class="todo-container">
           <button
             @click="toggleCompleted(toDo.id)"
-            :class="isTaskCompleted(toDo.id) ? 'task-completed' : 'complete'"
+            v-if="isTaskCompleted(toDo.id)"
           >
-            {{ isTaskCompleted(toDo.id) ? "restore task" : "completed" }}
+            <img src="src/assets/img/taskCompleted.svg" alt="" />
+          </button>
+          <button v-else
+            @click="toggleCompleted(toDo.id)"
+          
+          >
+            <img src="src/assets/img/completeTask.svg" alt="" />
+          </button>
+
+          <div class="text-container">
+            <h3 :class="isTaskCompleted(toDo.id) ? 'completed-task' : ''">
+              {{ toDo.text }}
+            </h3>
+          </div>
+        </div>
+
+        <div class="delete">
+          <button @click="deleteTask(i)">
+            <img src="src/assets/img/trash.svg" alt="" />
           </button>
         </div>
-        <div>
-          <button class="delete" @click="deleteTask(i)">delete</button>
-        </div>
       </div>
-    </div>
+    </main>
   </div>
 </template>
 
@@ -92,36 +112,132 @@ export default {
 };
 </script>
 
-<style scoped >
-.task {
+<style scoped lang="scss">
+.container {
+  background-color: #100f13;
+  color: white;
+  min-height: 100vh;
+  overflow: auto;
+}
+
+nav {
+  height: 70px;
+  background-color: #191920;
   display: flex;
-  justify-content: flex-end;
-  border: 1px solid grey;
-  margin: 20px auto;
-  width: 500px;
+  align-items: center;
+  border-bottom: 2px solid #474849;
 }
+
+main {
+  max-width: 700px;
+  margin: 0 auto;
+}
+
+.title {
+  margin-top: 120px;
+  margin-bottom: 80px;
+  display: flex;
+  h2 {
+    margin-left: 20px;
+    font-size: 35px;
+  }
+}
+
+::placeholder {
+  color: white;
+  font-size: 18px;
+}
+
+input {
+  width: 100%;
+  height: 50px;
+  border-radius: 8px;
+  background-color: #131318;
+  border: 3px solid #16161c;
+  padding-left: 60px;
+  color: white;
+  font-size: 20px;
+}
+
+.input-container {
+  position: relative;
+}
+
 button {
-  padding: 10px 20px;
-  border-radius: 15px;
+  all: unset;
 }
+
+.add-task-btn {
+  position: absolute;
+  top: 5px;
+  left: 5px;
+}
+
+.task {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  margin: auto;
+  width: 100%;
+  background: #191920;
+  border-radius: 8px;
+  margin-bottom: 18px;
+  h3 {
+     font-size: 20px;
+  } 
+}
+
+.task-count {
+  margin-top: 80px;
+  margin-bottom: 30px;
+  margin-left: 10px;
+  font-weight: bold;
+  font-size: 20px;
+  transition: display 0.9s;
+}
+
 .buttons {
   display: flex;
 }
+
 .text-container {
-  min-width: 200px;
+  width: 100%;
+  h3 {
+    margin-left: 10px;
+  }
 }
+
+.todo-container {
+  display: flex;
+  margin-left: 5px; 
+}
+
 .complete {
   background-color: green;
 }
+
 .delete {
-  background-color: red;
+  align-self: center;
+  margin-right: 5px;
+  transition: display 0.9s;
+  display: none;
 }
+
+.task:hover {
+  .delete {
+    display: block;
+  }
+  
+}
+
 .done {
   background-color: grey;
 }
+
 .task-completed {
   background-color: grey;
 }
+
 .completed-task {
   text-decoration: line-through;
 }
